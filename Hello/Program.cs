f27e95a83cfe5b2f9b8d3de7c35c98415bf5c8d9
kociumbas <kociumbas@outlook.com>
2016-02-16 00:28:44 +0100

    CSHello

---
+++ Hello/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("Write your name here: ");
+            string name = Console.ReadLine();
+            if (name.Length == 0)
+            {
+                Console.Error.WriteLine("\n\n\t*** Error: no name was given!\n\n");
+                return;
+            }
+            bool woman = (name.ToLower()[name.Length - 1] == 'a');
+            if (name == "Kuba" || name == "Barnaba") woman = false;
+//Console.WriteLine("Let me guess, you are " + (woman ? "a woman" : "a boy") + "!");
+            Console.WriteLine("Let me guess, you are {0}!", (woman ? "a woman" : "a boy"));
+//Show a message box with text (requires adding reference to Windows.Forms in solution explorer)
+            System.Windows.Forms.MessageBox.Show("Hello World!");
+            Console.WriteLine("Press Enter...");
+            Console.ReadLine();
+        }
+    }
+}
